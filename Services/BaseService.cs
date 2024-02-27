9a403ece04fdd2a523157e5d548ffb630a57fe9a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-27 18:15:27 +0300

    сделал базовый сервис

---
+++ Services/BaseService.cs
@@ -0,0 +1,17 @@
+﻿using BookApp.ADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Services
+{
+    public class BaseService
+    {
+        /// <summary>
+        /// Точка входа ADO. Используйте ее для взаимодействия с БД.
+        /// </summary>
+        protected readonly AdoEntities db = new AdoEntities();
+    }
+}
