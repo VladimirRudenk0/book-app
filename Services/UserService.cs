39b261d691e1f085285e28c02bab72494d97e5e7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-27 18:32:47 +0300

    сделал метод для получения пользователя по логину

---
+++ Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using BookApp.ADO;
+using System.Linq;
+
+namespace BookApp.Services
+{
+    public class UserService : BaseService
+    {
+        public C27_02_User_ GetUsetByLogin(string login)
+        {
+            return this.db.C27_02_User_.Where(u => u.UserLogin == login).First();
+        }
+    }
+}
